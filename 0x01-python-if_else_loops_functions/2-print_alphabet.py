c3a46c123aa44980f83b3aef292d56eb745a6e69
Shewanji <marrionwanjiru26@gmail.com>
2023-06-06 09:28:37 -0700

    print ASCII alphabet in lowercase

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-start = ord('a')
-end = ord('z') + 1
-
-for char_code in range(start, end):
-    print(chr(char_code), end='')
+for i in range(ord('a'), ord('z') + 1):
+    print('{:c}'.format(i), end='')
