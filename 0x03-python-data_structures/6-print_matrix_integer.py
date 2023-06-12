6a94911c60a475881c437fc6e1f90154b5a4c52c
Shewanji <marrionwanjiru26@gmail.com>
2023-06-12 01:51:00 -0700

    check whether column works

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,6 @@
 
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for num in row:
-            print("{:d}".format(num), end=" " if num != row[-1] else " ")
+        for column in row:
+            print("{:d}".format(column), end=' ')
         print()
