a4975a05a5c6de0f19028e6397e28ce2991ec901
Shewanji <marrionwanjiru26@gmail.com>
2023-07-10 12:09:31 -0700

    raise an exception error for BaseGeometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines a base geometry class BaseGeometry."""
+
+
+class BaseGeometry:
+    """this class represents a base geometry"""
+
+
+def area(self):
+    """
+    Calculate the area of the geometry.
+
+    Raises:
+    Exception: This method is not implemented in the base class.
+    """
+    raise Exception("area() is not implemented")
