75adf122b84d9e551d8d4dd4d80ad54e482f8c37
Shewanji <marrionwanjiru26@gmail.com>
2023-09-11 10:16:01 -0700

    script that prints a square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const argv = process.argv;
+
+if (argv[2] && Number(argv[2])) {
+  const x = parseInt(argv[2]);
+  let row = '';
+  for (let i = 0; i < x; i++) {
+    row += 'X';
+  }
+  for (let i = 0; i < x; i++) {
+    console.log(row);
+  }
+} else {
+  console.log('Missing size');
+}
