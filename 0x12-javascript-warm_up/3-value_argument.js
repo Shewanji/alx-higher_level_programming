ddd006437f673f2e13e849fa4b38dc7da3ebb90c
Shewanji <marrionwanjiru26@gmail.com>
2023-09-11 08:37:05 -0700

    do not use length

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
 const argv = process.argv;
 
-if (argv.length === 2) {
-  console.log('No argument');
+if (argv[2]) {
+  console.log(argv[2]);
 } else {
-  if (argv.length === 3) {
-    console.log('Argument found');
-  } else {
-    console.log('Arguments found');
-  }
+  console.log('No argument');
 }
