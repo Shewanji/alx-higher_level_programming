7bf842839ff0f1bd46612d5706436c2809c10aaf
Shewanji <marrionwanjiru26@gmail.com>
2023-09-16 14:23:36 -0700

    fix code should not be executed when imported

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -8,24 +8,24 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
-# Create a connection to the MySQL database
-engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-    sys.argv[1], sys.argv[2], sys.argv[3]))
+if __name__ == "__main__":
+    # Create a connection to the MySQL database
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]))
 
-# Create a session
-Session = sessionmaker(bind=engine)
-session = Session()
+    # Create a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-# Query and retrieve all State objects containing
-# the letter 'a', sorted by states.id
-states_with_a = session.query(State)\
-    .filter(State.name.like('%a%'))\
-    .order_by(State.id)\
-    .all()
+    # Query and retrieve all State objects containing
+    # the letter 'a', sorted by states.id
+    states_with_a = session.query(State)\
+        .filter(State.name.contains('a'))\
+        .all()
 
-# Print the State objects
-for state in states_with_a:
-    print("{}: {}".format(state.id, state.name))
+    # Print the State objects
+    for state in states_with_a:
+        print("{}: {}".format(state.id, state.name))
 
-# Close the session
-session.close()
+    # Close the session
+    session.close()
