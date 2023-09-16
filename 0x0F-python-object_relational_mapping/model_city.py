2b21727b2958a858afc8a3e18e83778e40ccbfea
Shewanji <marrionwanjiru26@gmail.com>
2023-09-16 14:59:23 -0700

    fix pycodestyle

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -6,6 +6,7 @@ Module for defining the City class.
 from sqlalchemy import Column, Integer, String, ForeignKey
 from model_state import Base
 
+
 class City(Base):
     """
     City class that inherits from Base.
