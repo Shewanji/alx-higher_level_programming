6417585fbc552f4252a16cc06804500dd37b1abd
Shewanji <marrionwanjiru26@gmail.com>
2023-09-29 10:04:36 -0700

    Python script that fetches https://alx-intranet.hbtn.io/status

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This script fetches https://alx-intranet.hbtn.io/status
+and displays information about the response.
+"""
+
+import requests
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+
+    response = requests.get(url)
+
+    print("Body response:")
+    print("\t- type:", type(response.text))
+    print("\t- content:", response.text)
