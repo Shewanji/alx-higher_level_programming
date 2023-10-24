b8d15486b7ea65c603f1a96aaf2a51a45129d4a3
Shewanji <marrionwanjiru26@gmail.com>
2023-10-24 12:08:32 -0700

    fix task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -15,7 +15,5 @@ const stringToWrite = process.argv[3];
 fs.writeFile(filePath, stringToWrite, 'utf-8', (err) => {
   if (err) {
     console.error(err);
-  } else {
-    console.log(`Successfully wrote the string to ${filePath}`);
   }
 });
