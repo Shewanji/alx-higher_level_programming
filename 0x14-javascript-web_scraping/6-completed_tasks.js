df6df98a119d2c60bdd1413a1a4e7871f37501b5
Shewanji <marrionwanjiru26@gmail.com>
2023-10-24 13:55:15 -0700

    script that computes the number of tasks completed by user id

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -11,32 +11,21 @@ if (process.argv.length !== 3) {
 const apiUrl = process.argv[2];
 
 // Send a GET request to the API to fetch todos
-request.get(apiUrl, (error, response, body) => {
+request.get(apiUrl, { json: true },(error, response, body) => {
   if (error) {
-    console.error('Error:', error);
-  } else if (response.statusCode === 200) {
-    const todos = JSON.parse(body);
-
-    // Create a map to store the count of completed tasks by user ID
-    const completedTaskCountByUser = new Map();
-
-    todos.forEach((task) => {
-      if (task.completed) {
-        const userId = task.userId;
-        if (completedTaskCountByUser.has(userId)) {
-          completedTaskCountByUser.set(userId, completedTaskCountByUser.get(userId) + 1);
-        } else {
-          completedTaskCountByUser.set(userId, 1);
-        }
-      }
-    });
-
-    // Print the users with completed tasks and their respective task counts
-    const result = {};
-    completedTaskCountByUser.forEach((count, userId) => {
-      result[userId] = count;
-    });
-
-    console.log(JSON.stringify(result, null, 2));
+    console.log(error);
+    return;
   }
-});
+
+  const tasksCompleted = {};
+  body.forEach((todo) => {
+    if (todo.completed) {
+      if (!tasksCompleted[todo.userId]) {
+        tasksCompleted[todo.userId] = 1;
+      } else {
+        tasksCompleted[todo.userId] += 1;
+      }
+    }
+  });
+  console.log(tasksCompleted);
+});	
